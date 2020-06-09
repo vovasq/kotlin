5c614c163ce3a0478bb11c723e7b9dea8c5cae76
vovasq <vova.balsutev@mail.ru>
2020-06-10 01:36:50 +0300

    [Leaking_This] revert not stashed changes

--- compiler/fir/analysis-tests/tests/org/jetbrains/kotlin/fir/FirDiagnosticsWithLightTreeTestGenerated.java
+++ compiler/fir/analysis-tests/tests/org/jetbrains/kotlin/fir/FirDiagnosticsWithLightTreeTestGenerated.java
@@ -943,10 +943,6 @@ public class FirDiagnosticsWithLightTreeTestGenerated extends AbstractFirDiagnos
                 runTest("compiler/fir/analysis-tests/testData/resolve/diagnostics/leakingThis/inlineAndLambdas.kt");
             }
 
-            @TestMetadata("interfaces.kt")
-            public void testInterfaces() throws Exception {
-                runTest("compiler/fir/analysis-tests/testData/resolve/diagnostics/leakingThis/interfaces.kt");
-            }
 
             @TestMetadata("manyInitSections.kt")
             public void testManyInitSections() throws Exception {
